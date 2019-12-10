06d133385cca5c55919c6c819a47487a66543dc1
Justinely205 <55204849+Justinely205@users.noreply.github.com>
2019-12-10 14:22:08 -0500

    adj

---
+++ ACT01/index.js
@@ -0,0 +1,14 @@
+//$(document).ready(function(){
+//	$(".act1").hover(function(){
+//		$(".info").append("hello");
+//	}).mouseleave(function() {
+//		$(".info").append("bye")
+//	})
+//})
+
+
+$(document).ready(function(){
+  $(".act1").click(function(){
+    window.open('https://justinely205.github.io/IM-WEB-2019/ACT01/CSS-typeface/css-typeface.html');
+  });
+});
